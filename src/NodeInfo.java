cf611c725cbe09db57ea3f9143a6644c5b856b99
NazifaC <nazifa.chowdhury@city.ac.uk>
2024-03-26 15:05:19 +0000

    created a nodeInfo class

---
+++ src/NodeInfo.java
@@ -0,0 +1,11 @@
+public class NodeInfo {
+
+    String nodeName;
+    String nodeAddress;
+
+    public NodeInfo(String nodeName, String nodeAddress) {
+        this.nodeName = nodeName;
+        this.nodeAddress = nodeAddress;
+
+    }
+}
